45a2436cde7e4844749712ee5bc19a5ac0ab472b
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-01-07 13:46:53 +0330

    Update for installing the snap apps

--- main.sh
+++ main.sh
@@ -18,71 +18,40 @@ else
     LOG_FILE="$LOG_DIR/logs.log"
 fi
 
-touch "$LOG_FILE"
-
 exec > >(tee -a "$LOG_FILE") 2>&1
 
 echo "Created at: $(date +"%Y-%m-%d_%H-%M-%S")"
 
 install_package() {
     local package="$1"
-    echo "Installing $package..."
-    if ! sudo apt-get install -y "$package"; then
-        echo "Failed to install $package"
-        exit 1
+    local manager="$2"
+
+    if [[ "$manager" == "apt" ]]; then
+        echo "Installing $package..."
+        if ! sudo apt-get install -y "$package"; then
+            echo "Failed to install $package"
+            exit 1
+        fi
+    elif [[ "$manager" == "snap" ]]; then
+        echo "Installing $package..."
+        if ! sudo snap install "$package"; then
+            echo "Failed to install $package"
+            exit 1
+        fi
     fi
     echo "$package installed"
 }
 
-# Check if Git is installed
-# if command -v git >/dev/null 2>&1; then
-#     log_and_show "Git is already installed."
-#     log_and_show "Do you want to configure Git? (yes/no)"
-#     read -r configure_git </dev/tty
-
-#     if [[ "$configure_git" == "yes" ]]; then
-#         log_and_show "Enter your Git user name:"
-#         read -r git_user_name </dev/tty
-#         log_and_show "Enter your Git user email:"
-#         read -r git_user_email </dev/tty
-
-#         git config --global user.name "$git_user_name"
-#         git config --global user.email "$git_user_email"
-
-#         log_and_show "Git has been configured with:"
-#         log_and_show "User Name: $git_user_name"
-#         log_and_show "User Email: $git_user_email"
-#     else
-#         log_and_show "Git configuration skipped."
-#     fi
-# else
-#     log_and_show "Git is not installed. Would you like to install it? (yes/no)"
-#     read -r install_git </dev/tty
-#     if [[ "$install_git" == "yes" ]]; then
-#         log_and_show "Git has been installed. You can run the script again to configure Git."
-#     else
-#         log_and_show "Skipping Git installation."
-#     fi
-# fi
-
-if [ -f "$CONF_DIR" ]; then
-    source "$CONF_DIR"
-else
-    echo "packages.conf file not found!" >&2
-    exit 1
-fi
-
-echo "Updating system..."
-# sudo apt-get update && sudo apt-get upgrade -y
-
-echo "Installing desired packages:"
-for package in "${packages[@]}"; do
-    echo "- Installing $package"
-    install_package "$package"
-
-    # Check if the current package is "git"
+git_config() {
+    local package="$1"
     if [[ "$package" == "git" ]]; then
-        echo "Do you want to (re)configure Git? (yes/no)"
+        if [ -t 0 ]; then
+            echo "Do you want to (re)configure Git? (yes/no)"
+            read -r configure_git </dev/tty
+        else
+            echo "Not running in an interactive shell. Skipping Git configuration."
+            return
+        fi
         read -r configure_git </dev/tty
 
         if [[ "$configure_git" == "yes" ]]; then
@@ -101,4 +70,31 @@ for package in "${packages[@]}"; do
             echo "Git configuration skipped."
         fi
     fi
+}
+
+if [ -f "$CONF_DIR" ]; then
+    if ! source "$CONF_DIR"; then
+        echo "Error: Failed to source $CONF_DIR. Check the file for syntax errors." >&2
+        exit 1
+    fi
+else
+    echo "Error: $CONF_DIR file not found!" >&2
+    exit 1
+fi
+
+echo "Updating system..."
+# sudo apt-get update && sudo apt-get upgrade -y
+
+echo "Installing Official packages:"
+for package in "${apt_apps[@]}"; do
+    echo "- Installing $package"
+    install_package "$package" "apt"
+    git_config "$package"
 done
+
+echo "Installing Snap packages:"
+for package in "${snap_apps[@]}"; do
+    echo "- Installing $package"
+    install_package "$package" "snap"
+done
+
